961d8f899ec1e87bc097f66fcc57f65faa838d14
mrsuit0114 <woosa0114@gmail.com>
2021-12-07 22:46:26 +0900

    givefood
    view model

--- src/model/GiveFoodModel.java
+++ src/model/GiveFoodModel.java
@@ -1,5 +1,56 @@
 package model;
 
-public class GiveFoodModel {
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Observable;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class GiveFoodModel extends Observable{
+	private Deque<Integer> time = new LinkedList<>();
+	
+	public void addTime() {
+		Scanner scan = new Scanner(System.in);
+		int hour = -1;
+		while(hour<0 || hour>24) {
+			System.out.print("원하는 시간을 입력하시오.\n시: ");
+			hour = scan.nextInt();
+		}
+		int min = -1;
+		while(min<0 || min>60) {
+			System.out.print("분: ");
+			min = scan.nextInt();
+		}
+		int n = hour*60 + min;
+		time.add(n);
+		setChanged();
+		notifyObservers();
+	}
+	
+	public void resetTime() {
+		time = new LinkedList<Integer>();
+		System.out.println("시간 설정을 초기화 합니다.");
+	}
+	
+	public void delTime() {
+		time.removeLast();
+	}
+	
+	public void giveFood() {
+		System.out.println("사료를 줍니다.");
+	}
+	
+	public void checkTime() {
+		System.out.println("현재 설정된 시간입니다.");
+		Iterator it = time.iterator();
+		if(it.hasNext()!=true)
+			System.out.println("설정된 시간 정보가 없습니다.");
+		while(it.hasNext()) {
+			int temp = (int) it.next();
+			System.out.printf("%d시 "+"%d분\n",temp/60,temp%60);
+		}
+	}
 
 }
--- src/view/GiveFoodView.java
+++ src/view/GiveFoodView.java
@@ -1,5 +1,37 @@
 package view;
 
-public class GiveFoodView {
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+import model.GiveFoodModel;
+import model.MainModel;
+
+public class GiveFoodView implements Observer{
+
+	private GiveFoodModel gm = null;
+	private MainModel mm =null;
+	
+	public GiveFoodView(MainModel mm, GiveFoodModel gm) {
+		System.out.println("give food menu를 선택하시오\n 1)바로 주기 2)주는 시간 추가 3)시간 초기화 4)메인화면으로 나가기");
+		Scanner scn = new Scanner(System.in);
+		gm.addObserver(this);
+		int choice = scn.nextInt();
+		switch (choice) {
+		case 1 : gm.giveFood(); mm.setSceneNum(2); break;
+		case 2 : gm.checkTime(); gm.addTime(); mm.setSceneNum(2); break;
+		case 3 :gm.resetTime(); mm.setSceneNum(2); break;
+		default : mm.setSceneNum(2); break;
+		}
+		this.gm = gm;
+		this.mm=mm;
+		scn.close();
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("시간 설정이 변경되었습니다.");
+	}
+	
 
 }
